26e69eac7ee4484de49167c8ed3282d0c8db6c15
FilonWin <rzadki22@gmail.com>
2021-03-01 19:07:59 +0100

    Added .html template (just for test)

--- manage_library/urls.py
+++ manage_library/urls.py
@@ -4,4 +4,8 @@ from . import views
 
 urlpatterns = [
     path('', views.main_manage_library, name="main_manage_library"),
+    path('', views.main_manage_library, name="show-book-collection"),
+    path('', views.main_manage_library, name="add-new-book"),
+    path('', views.main_manage_library, name="remove-book"),
+    path('', views.main_manage_library, name="add-bookcase"),
 ]
--- manage_library/views.py
+++ manage_library/views.py
@@ -2,5 +2,7 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
+# TODO decorator of log in
 def main_manage_library(request):
-    return HttpResponse("Czesc!")
+    pass
+    return render(request, template_name="manage_library/main_manage_library.html")
