ab6c21f3695df640925cd849665933b08f0a68ab
Malgorzata Tutaj <malgorzata.tutaj@gmail.com>
2021-03-02 23:47:08 +0100

    created models: Book, Shelf, Bookcase, Room, Category

--- manage_library/models.py
+++ manage_library/models.py
@@ -1,25 +1,29 @@
 from django.db import models
 
+
 class Category(models.Model):
     name = models.CharField(max_length=20)
-    id = models.CharField(max_length=100, unique=True)
+
+
+class Room(models.Model):
+    name = models.CharField(max_length=20)
+
+
+class Bookcase(models.Model):
+    name = models.CharField(max_length=20)
+    room = models.ForeignKey(Room, max_length=130)
+
+
+class Shelf(models.Model):
+    bookcase = models.ForeignKey(Bookcase, max_length=20)
 
 
 class Book(models.Model):
-    id = models.CharField(max_length=100, unique=True)
     title = models.CharField(max_length=180, required=True)
     author = models.CharField(max_length=100, required=True)
     isbn_number = models.CharField(max_length=13)
     note = models.CharField(max_length=255)
     category = models.ForeignKey(Category, models.SET_NULL)
     cover = models.ImageField()
-    shelf = models.ForegnKey(Shelf, models.SET_NULL)
+    shelf = models.ForeignKey(Shelf, models.SET_NULL)
     lending_status = models.BooleanField()
-
-class Shelf(models.Model):
-    bookcase = models.ForeignKey(Shelf, max_length=20)
-    id = models.CharField(max_length=100, unique=True)
-
-
-
-
