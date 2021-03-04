b17a7ae77a0d65f2b28ba86b6d57c62a7a1064d8
FilonWin <rzadki22@gmail.com>
2021-03-04 17:54:14 +0100

    Book model modified

--- manage_library/models.py
+++ manage_library/models.py
@@ -1,3 +1,4 @@
+from django.core.validators import RegexValidator
 from django.db import models
 
 
@@ -11,7 +12,7 @@ class Room(models.Model):
 
 class Bookcase(models.Model):
     name = models.CharField(max_length=20)
-    room = models.ForeignKey(Room, max_length=130)
+    room = models.ForeignKey(Room, max_length=130) #TODO trzeba jeszcze on_delete
 
 
 class Shelf(models.Model):
@@ -19,11 +20,18 @@ class Shelf(models.Model):
 
 
 class Book(models.Model):
-    title = models.CharField(max_length=180, required=True)
-    author = models.CharField(max_length=100, required=True)
-    isbn_number = models.CharField(max_length=13)
-    note = models.CharField(max_length=255)
-    category = models.ForeignKey(Category, models.SET_NULL)
+    title = models.CharField(max_length=180, required=True, blank=False)
+    author = models.CharField(max_length=100, required=True, blank=False)
+    isbn_number = models.CharField(validator=[
+        RegexValidator(regex='^[0-9]{10}$', message="Wrong ISBN length",
+        RegexValidator(regex='^[0-9]{13}$', message="Wrong ISBN length"
+    ])
+    note = models.TextField()
     cover = models.ImageField()
-    shelf = models.ForeignKey(Shelf, models.SET_NULL)
-    lending_status = models.BooleanField()
+    category = models.ForeignKey(Category, models.SET_NULL) #TODO trzeba jeszcze on_delete
+    shelf = models.ForeignKey(Shelf, models.SET_NULL) #TODO trzeba jeszcze on_delete
+    lending_status = models.CharField(choices=[
+        (lent, "lent",),
+        (free, "free"),
+    ], default=free
+    )
