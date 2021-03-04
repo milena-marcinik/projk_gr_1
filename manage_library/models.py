1a677b9718f0e32208eb0beb87d0aa8a40b36bc6
FilonWin <rzadki22@gmail.com>
2021-03-04 18:12:30 +0100

    warnings after manage.py check were conducted

--- manage_library/models.py
+++ manage_library/models.py
@@ -22,15 +22,15 @@ class Shelf(models.Model):
 class Book(models.Model):
     title = models.CharField(max_length=180, blank=False)
     author = models.CharField(max_length=100, blank=False)
-    isbn_number = models.CharField(validators=[
-        RegexValidator(regex='^[0-9]{10}$', message="Wrong ISBN length"),
-        RegexValidator(regex='^[0-9]{13}$', message="Wrong ISBN length"),
+    isbn_number = models.CharField( max_length=13,validators=[
+            RegexValidator(regex='^[0-9]{10}$', message="Wrong ISBN length"),
+            RegexValidator(regex='^[0-9]{13}$', message="Wrong ISBN length"),
     ])
     note = models.TextField()
     cover = models.ImageField()
     category = models.ForeignKey(Category, on_delete=models.SET_NULL, null=True)
     shelf = models.ForeignKey(Shelf, on_delete=models.SET_NULL, null=True)
-    lending_status = models.CharField(choices=[
+    lending_status = models.CharField(max_length=4, choices=[
         ("lent", "lent",),
         ("free", "free",),
     ], default="free")
