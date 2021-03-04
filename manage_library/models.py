a3deed5513886bf7914058fdf2a26af8f6290eb7
FilonWin <rzadki22@gmail.com>
2021-03-04 17:57:35 +0100

    Auxiliaries added

--- manage_library/models.py
+++ manage_library/models.py
@@ -12,7 +12,7 @@ class Room(models.Model):
 
 class Bookcase(models.Model):
     name = models.CharField(max_length=20)
-    room = models.ForeignKey(Room, max_length=130) #TODO trzeba jeszcze on_delete
+    room = models.ForeignKey(Room, max_length=130)  # TODO trzeba jeszcze on_delete
 
 
 class Shelf(models.Model):
@@ -23,15 +23,14 @@ class Book(models.Model):
     title = models.CharField(max_length=180, required=True, blank=False)
     author = models.CharField(max_length=100, required=True, blank=False)
     isbn_number = models.CharField(validator=[
-        RegexValidator(regex='^[0-9]{10}$', message="Wrong ISBN length",
-        RegexValidator(regex='^[0-9]{13}$', message="Wrong ISBN length"
+        RegexValidator(regex='^[0-9]{10}$', message="Wrong ISBN length"),
+        RegexValidator(regex='^[0-9]{13}$', message="Wrong ISBN length"),
     ])
     note = models.TextField()
     cover = models.ImageField()
-    category = models.ForeignKey(Category, models.SET_NULL) #TODO trzeba jeszcze on_delete
-    shelf = models.ForeignKey(Shelf, models.SET_NULL) #TODO trzeba jeszcze on_delete
+    category = models.ForeignKey(Category, models.SET_NULL)  # TODO trzeba jeszcze on_delete
+    shelf = models.ForeignKey(Shelf, models.SET_NULL)  # TODO trzeba jeszcze on_delete
     lending_status = models.CharField(choices=[
         (lent, "lent",),
         (free, "free"),
-    ], default=free
-    )
+    ], default=free)
