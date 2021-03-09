3c062ef1639d31736462cd1fcbf1fab46eb33b71
Milena <milena.marcinik@gmail.com>
2021-03-10 00:35:51 +0100

    feature(PG-20) admin can add new rooms

--- manage_library/urls.py
+++ manage_library/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 
 from . import views
+from .views import RoomListView, RoomCreateView
 
 urlpatterns = [
     path('', views.main_manage_library, name="main_manage_library"),
@@ -9,8 +10,8 @@ urlpatterns = [
     path('a/', views.main_manage_library, name="show-book-collection"),
     path('b/', views.main_manage_library, name="add-new-book"),
     path('c/', views.main_manage_library, name="remove-book"),
-    path('room/', views.main_manage_library, name="all-rooms"),
-    path('d/', views.main_manage_library, name="add-room"),
+    path('room/', RoomListView.as_view(template_name='manage_library/rooms_all.html'), name="all-rooms"),
+    path('addroom/', RoomCreateView.as_view(template_name='manage_library/add_room.html'), name="add-room"),
     path('e/', views.main_manage_library, name="add-bookcase"),
     path('f/', views.main_manage_library, name="add-shelf"),
 
--- manage_library/views.py
+++ manage_library/views.py
@@ -1,9 +1,29 @@
+from django.contrib import messages
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+
+from django.views.generic import ListView, CreateView
+
+from manage_library.models import Room
 
 
-# Create your views here.
 # TODO decorator of log in
 def main_manage_library(request):
     pass
     return render(request, template_name="manage_library/main_manage_library.html")
+
+
+class RoomListView(ListView):
+    model = Room
+    context_object_name = 'rooms_all'  # your own name for the list as a template variable
+    queryset = Room.objects.all()
+    template_name = 'manage_library/rooms_all.html'  # Specify your own template name/location
+
+
+class RoomCreateView(CreateView):
+    model = Room
+    fields = ['name']
+    success_url = '/room/'
+
+    def form_valid(self, form):
+        return super().form_valid(form)
