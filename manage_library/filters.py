b703ae1ceb6f8b9d3da25684e4b55e0f7b56f972
Milena <milena.marcinik@gmail.com>
2021-03-13 21:59:03 +0100

    room - filter

--- manage_library/filters.py
+++ manage_library/filters.py
@@ -1,12 +1,15 @@
 import django_filters
-from django.utils.translation import ugettext_lazy as _
 from .models import Book
 
 
 class BookFilter(django_filters.FilterSet):
-    title = django_filters.CharFilter(lookup_expr='icontains')
-    author = django_filters.CharFilter(lookup_expr='icontains')
+    title = django_filters.CharFilter(lookup_expr='icontains', label="Title")
+    author = django_filters.CharFilter(lookup_expr='icontains', label="Author")
 
     class Meta:
         model = Book
         fields = ('category', 'shelf__bookcase__room')
+
+    def __init__(self, *args, **kwargs):
+        super(BookFilter, self).__init__(*args, **kwargs)
+        self.filters['shelf__bookcase__room'].label = "Room"
