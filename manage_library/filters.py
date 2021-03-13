e861a75b0fc79782e1dc414a10b11b4540f2a51e
Milena <milena.marcinik@gmail.com>
2021-03-13 21:03:28 +0100

    feature(PG-39)filter by title and author

--- manage_library/filters.py
+++ manage_library/filters.py
@@ -4,6 +4,8 @@ from .models import Book
 
 
 class BookFilter(django_filters.FilterSet):
+    title = django_filters.CharFilter(lookup_expr='icontains')
+    author = django_filters.CharFilter(lookup_expr='icontains')
 
     class Meta:
         model = Book
